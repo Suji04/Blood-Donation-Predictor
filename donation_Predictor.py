c841eeed432ca662f34144c7aae05190bef010f4
Sujan Dutta <33905582+Suji04@users.noreply.github.com>
2018-10-29 09:31:09 +0530

    Update donation_Predictor.py

--- donation_Predictor.py
+++ donation_Predictor.py
@@ -21,32 +21,15 @@ sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.fit_transform(X_test)
 
-
-# fitting random forest classifier in traning set
-'''from sklearn.ensemble import RandomForestClassifier
-classifier = RandomForestClassifier(n_estimators=10, criterion="entropy")
-classifier.fit(X_train,y_train)'''
-
-
-# fitting logistic reg in traning set
-'''from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state = 0)
-classifier.fit(X_train, y_train)'''
-
 # Fitting K-NN to the Training set
 from sklearn.neighbors import KNeighborsClassifier
 classifier = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
 classifier.fit(X_train, y_train)
 
-# fitting naive bayes classifier in traning set
-'''from sklearn.naive_bayes import GaussianNB
-classifier = GaussianNB()
-classifier.fit(X_train,y_train)'''
-
-# predicting test set results
+# Predicting test set results
 y_pred = classifier.predict(X_test)
 
-# creating confusion matrix
+# Creating confusion matrix
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test,y_pred)
 
